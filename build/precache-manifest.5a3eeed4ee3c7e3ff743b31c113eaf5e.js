7c16440889c1c4db374d073fd6e623b3a4f3fe36
ayushkul <akcool.ayush.svm@gmail.com>
2022-01-24 16:28:16 +0530

    build commit

---
+++ build/precache-manifest.5a3eeed4ee3c7e3ff743b31c113eaf5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1859e13d4113d474b401823e95c76c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f1b766458bd98c2c7e",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "12510698cbf11dce24c5",
+    "url": "/static/css/main.e21ba8a2.chunk.css"
+  },
+  {
+    "revision": "50f1b766458bd98c2c7e",
+    "url": "/static/js/2.61ef55a1.chunk.js"
+  },
+  {
+    "revision": "12510698cbf11dce24c5",
+    "url": "/static/js/main.b8c8fd38.chunk.js"
+  },
+  {
+    "revision": "e57324a108cc3a90c0c5",
+    "url": "/static/js/runtime~main.87667b26.js"
+  }
+]);
