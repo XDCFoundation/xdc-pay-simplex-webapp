71cd35c6201897e9fefd7314056abe2362152031
ayushkul <akcool.ayush.svm@gmail.com>
2022-02-03 09:41:55 +0530

    Dev Commit

--- src/modules/simplex/index.js
+++ src/modules/simplex/index.js
@@ -1,14 +1,8 @@
-import React, {useEffect} from "react";
+import React from "react";
 
 const SimplexWebPage = () => {
-    useEffect(() => {
-        if (window.simplex)
-            window.simplex.updateCryptoCurrency("XDC");
-    })
     return (
         <div className="simplex">
-            <iframe title="simplex" is="x-frame-bypass" src={process.env.REACT_APP_IS_PROD_ENV === "true"
-                ? "/simplex.html" : "/simplex-sandbox.html"}/>
         </div>)
 }
 export default SimplexWebPage;
